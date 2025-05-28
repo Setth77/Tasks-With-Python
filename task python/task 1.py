d61ae7ddb85570ff0ce0b09500cc406f4c80eb0d
Setth77 <lexithayate.2003@gmail.com>
2025-05-28 19:57:03 -0300

    hello

--- task python/task 1.py
+++ task python/task 1.py
@@ -1,9 +1,27 @@
+print("Cardápio:")
+print("1 - Pastel (R$12)")
+print("2 - Açaí (R$11)")
+print("3 - X-Burger (R$15)")
+print("Digite 'q' para sair e ver o total.\n")
+
+cardapio = {
+    '1': ('pastel', 12),
+    '2': ('acai', 11),
+    '3': ('xburger', 15)
+}
+nome, preco = cardapio['1']
+
+total = 0 
+
 while True:
-    comando = str(input("digite uma opção(ou q pra sair): "))
-    if comando.lower() == 'q':
-        print("encerrando... ")
+    comando = input("digite uma opção entre 1-3: ").lower()
+    
+    if comando == 'q':
+        print(f"encerrando... total do pedido: R${total}")
         break
+    if comando in cardapio:
+        nome, preco = cardapio[comando]
+        total += preco
+        print(f"voce pediu '{comando}' por R${preco}. ")
     else:
-        while True:
-          print(f"voce digitou: '{comando}'")
-          break
+        print("opcao invalida, tente novamente.")
