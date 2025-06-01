1fc714003a370383c93896c59420f8008ec2ce7e
Setth77 <lexithayate.2003@gmail.com>
2025-05-31 21:14:40 -0300

    adicionando resumo do pedido

--- task python/task 1.py
+++ task python/task 1.py
@@ -12,16 +12,28 @@ cardapio = {
 nome, preco = cardapio['1']
 
 total = 0 
+items_pedidos = {}
+
 
 while True:
     comando = input("digite uma opção entre 1-3: ").lower()
     
     if comando == 'q':
-        print(f"encerrando... total do pedido: R${total}")
+        print(f"encerrando... total do pedido: R${total}\n")
+        for nome, (preco, quantidade) in items_pedidos.items():
+            subtotal = preco * quantidade
+            print(f"{nome} - {quantidade}x - R${preco} cada - Subtotal: R${subtotal}")
+        print(f"\nTotal do pedido: R${total}")
         break
+    
+    
     if comando in cardapio:
         nome, preco = cardapio[comando]
         total += preco
-        print(f"voce pediu '{comando}' por R${preco}. ")
+        if nome in items_pedidos:
+            items_pedidos[nome][1] += 1
+        else:
+            items_pedidos[nome] = [preco, 1]
+        print(f"voce pediu '{nome}' por R${preco}. ")
     else:
         print("opcao invalida, tente novamente.")
